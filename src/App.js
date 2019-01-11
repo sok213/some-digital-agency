87cb617e823a825b075502c68ca5f03b83dd4402
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2019-01-10 17:15:24 -0800

    Create files for all components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Jumbotron from './components/Jumbotron/Jumbotron';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Some Digital Agency</h1>
+        <Jumbotron />
       </div>
     );
   }
