cb92aba2359db55c9f6cc135c85c24e25c967fc5
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2019-01-10 17:25:49 -0800

    Create ActiveSection component and move child component folders into it.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Jumbotron from './components/Jumbotron/Jumbotron';
+import NavBar from './components/NavBar/NavBar';
+import ActiveSection from './components/ActiveSection/ActiveSection';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Jumbotron />
+        <NavBar />
+        <ActiveSection />
       </div>
     );
   }
