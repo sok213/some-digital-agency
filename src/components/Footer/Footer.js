ba46bd94fb8827d16a736e8fe06205f71a1e9169
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2019-01-13 16:41:14 -0800

    Improve footer styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,71 @@
 import React, { Component } from 'react';
 import s from './Footer.module.scss';
+import { Animated } from 'react-animated-css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+    faLinkedin,
+    faMedium
+} from '@fortawesome/free-brands-svg-icons';
+import { 
+    faCode 
+} from '@fortawesome/free-solid-svg-icons';
 
 class Footer extends Component {
     render() {
         return (
             <div className={s.footerContainer}>
                 <div className={s.footerContent}>
-                    <h3>MORE</h3>
-                    <ul className={s.linksList}>
-                        <li><a href="#"><p>Portfolio</p></a></li>
-                        <li><a href="#"><p>LinkedIn</p></a></li>
-                        <li><a href="#"><p>Medium</p></a></li>
-                        <li><a href="#"><p>Mail</p></a></li>
-                    </ul>
-                    <div className={s.credit}>
-                        <p>Some Digital Agency copyright 2019</p>
+                    {/* <h3>MORE</h3> */}
+                    <div className={s.col}>
+                        <ul className={s.linksList}>
+                            <h4>Some Stuff</h4>
+                            <li>
+                                <a href="#">
+                                    <p>Portfolio<FontAwesomeIcon className={s.faIcon} icon={faCode} /></p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <p>LinkedIn<FontAwesomeIcon className={s.faIcon} icon={faLinkedin} /></p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <p>Medium<FontAwesomeIcon className={s.faIcon} icon={faMedium} /></p>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
+
+                    <div className={s.col}>
+                    <ul className={s.linksList}>
+                            <h4>No One Clicks on These</h4>
+                            <li>
+                                <a href="#">
+                                    <p>API</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <p>License</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className={s.col}>
+                        <ul className={s.linksList}>
+                            <h4>Some More Stuff</h4>
+                            <li><a href="#"><p>Contact</p></a></li>
+                            <li><a href="#"><p>Status</p></a></li>
+                            <li><a href="#"><p>Help</p></a></li>
+                        </ul>
+                    </div>
+                    
+
+                </div>
+                <div className={s.credit}>
+                    <p>Some Digital Agency copyright 2019</p>
                 </div>
             </div>
         );
